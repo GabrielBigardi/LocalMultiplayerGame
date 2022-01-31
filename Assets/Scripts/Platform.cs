4a8b791e7450064e52687eaadfa59a793d52bad1
Gabriel Bigardi <42253181+GabrielBigardi@users.noreply.github.com>
2022-01-31 18:19:59 -0300

    Update Platform.cs

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -8,11 +8,19 @@ public class Platform : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            if(collision.gameObject.GetComponent<PlayerInputHandler>().mov.y == -1f)
+            if (collision.gameObject.GetComponent<PlayerInputHandler>().mov.y == -1f)
             {
-                Physics2D.IgnoreCollision(collision.gameObject.GetComponentInChildren<Collider2D>(), GetComponent<Collider2D>());
+                Collider2D ignoredCollider = collision.gameObject.GetComponentInChildren<Collider2D>();
+                Physics2D.IgnoreCollision(ignoredCollider, GetComponent<Collider2D>());
+                StartCoroutine(ResetColisionForCollider_CR(ignoredCollider));
             }
         }
+    }
+
+    public IEnumerator ResetColisionForCollider_CR(Collider2D collision)
+    {
+        yield return new WaitForSeconds(1f);
+        Physics2D.IgnoreCollision(collision, GetComponent<Collider2D>(), false);
     }
 
     //private void OnCollisionExit2D(Collision2D collision)
