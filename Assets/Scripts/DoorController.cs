e096f4f94882284aa68ac8b6fa4f0ea97558d2e3
Gabriel Bigardi <42253181+GabrielBigardi@users.noreply.github.com>
2022-02-07 13:01:44 -0300

    Dotween Import + Testing

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoorController : MonoBehaviour
+{
+    private void Start()
+    {
+        GameEvents.Instance.onDoorwayTriggerEnter += DoorwayOpen;
+        GameEvents.Instance.onDoorwayTriggerExit += DoorwayExit;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.Instance.onDoorwayTriggerEnter -= DoorwayOpen;
+        GameEvents.Instance.onDoorwayTriggerExit -= DoorwayExit;
+    }
+
+    private void DoorwayOpen()
+    {
+        transform.DOMoveY(-3.6875f,2f).SetEase(Ease.OutBounce);
+    }
+
+    private void DoorwayExit()
+    {
+        transform.DOMoveY(10.5f, 1f);
+    }
+}
---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public event Action onDoorwayTriggerEnter;
+    public event Action onDoorwayTriggerExit;
+
+    public void DoorwayTriggerEnter()
+    {
+        onDoorwayTriggerEnter?.Invoke();
+    }
+
+    public void DoorwayTriggerExit()
+    {
+        onDoorwayTriggerExit?.Invoke();
+    }
+}
